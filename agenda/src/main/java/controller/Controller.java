d463408fba30c88e51d950e991a88ed24879c412
LuizHLG <luizlelek08@gmail.com>
2023-11-19 16:32:18 -0300

    Camada model configurada

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO;
 
 /**
  * Servlet implementation class Controller
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/Controller","/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	DAO dao = new DAO();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +31,7 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 }
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DAO {
 
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "Luizh123$";
+	
+	private Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
 }
