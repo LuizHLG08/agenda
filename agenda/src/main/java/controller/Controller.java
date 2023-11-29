a00a068e52325366888fc282560a1202c6fc19c6
LuizHLG <luizlelek08@gmail.com>
2023-11-29 16:34:18 -0300

    controller config

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -8,30 +8,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
+
 @WebServlet(urlPatterns = {"/Controller","/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	DAO dao = new DAO();
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public Controller() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		String action = request.getServletPath();
+		System.out.println(action);
 		
+		if(action.equals("/main")) {
+			contacts(request, response);
+		}
 	}
+	
+	
+	protected void contacts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
+	
 
 }
---
+++ agenda/src/main/webapp/agenda.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<title>Agenda de contatos</title>
+<link rel="stylesheet" href="style.css">
+<link rel="icon" href="imagens/telefone_img.png">
+</head>
+<body>
+	<h1>Agenda de Contatos</h1>
+	<a class="botao1" href="">Novo contato</a>
+</body>
+</html>
