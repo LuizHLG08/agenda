e7b39100890a242258ccfe4e82c1697160937753
LuizHLG <luizlelek08@gmail.com>
2024-05-01 00:58:55 -0300

    get request

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -1,11 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import model.DAO;
 import model.JavaBeans;
 
@@ -36,7 +39,7 @@ public class Controller extends HttpServlet {
 	
 	
 	protected void contacts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+		ArrayList<JavaBeans> list = dao.listContacts();
 	}
 	
 	protected void newContact(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -3,9 +3,10 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DAO {
-
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
@@ -31,6 +32,7 @@ public class DAO {
 		String create = "insert into contacts (name, phone, email) values (?,?,?)";
 		
 		try {
+			
 			Connection con = connect();
 			
 			PreparedStatement pst = con.prepareStatement(create);
@@ -39,8 +41,38 @@ public class DAO {
 			pst.setString(3, contact.getEmail());
 			pst.executeUpdate();
 			con.close();
+			
+		} catch (Exception e) {
+			
+			System.out.println(e);
+		}
+	}
+	
+	public ArrayList<JavaBeans> listContacts() {
+		
+		ArrayList<JavaBeans> contacts = new ArrayList<>();
+		
+		String read = "select * from contacts order by name";
+		
+		try {
+			Connection con = connect();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+			
+			while (rs.next()) {
+				String idcon =  rs.getString(1);
+				String name =  rs.getString(2);
+				String phone =  rs.getString(3);
+				String email =  rs.getString(4);
+				
+				contacts.add(new JavaBeans(idcon, name, phone, email));
+			}
+			
+			con.close();
+			return contacts;
 		} catch (Exception e) {
 			System.out.println(e);
+			return null;
 		}
 	}
 }
